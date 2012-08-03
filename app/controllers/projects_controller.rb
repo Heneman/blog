771c4a5bd8d835705b9ccb0ccf02ef8d40115998
Heneman <kyle.heneman@gmail.com>
2012-08-03 17:52:39 -0500

    Restyled 'pagination', added class for 'bash' in 'application.sass', removed 'Projects' controller/views

--- app/controllers/projects_controller.rb
+++
@@ -1,8 +0,0 @@
-# TODO -
-# class ProjectsController < ApplicationController
-#   before_filter :authenticate_action
-
-#   def index
-#     @entries = Entry.order('created_at desc').where(:category => 'projects').paginate(:page => params[:page], :per_page => 10)
-#   end
-# end
